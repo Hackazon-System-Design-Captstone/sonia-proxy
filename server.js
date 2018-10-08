1c642577996c566a7909cbae2d257473b7cf5e94
Neda Rohani <sonia@Nedas-MacBook-Pro.local>
2018-10-08 14:57:29 -0700

    connect server, perform stress-test

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-require('dotenv').config();
+require('newrelic');
+const axios = require('axios');
+// require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use( express.static(path.join(__dirname, 'public')));
+
+app.get('/get', (req, res) => {
+ 
+    axios.get(`http://localhost:9002/get?id=${req.query.id}`)
+    .then(({data}) => {
+      res.send(data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  } 
+);
 
 app.listen(port, () => {
-  console.log(`Proxy server listening on port ${port}...`);
+  console.log(`Proxy server listening on port ${port} `);
 });
